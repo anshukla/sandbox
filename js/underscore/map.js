f10c94642beaa0e5086d36be289de017e3e8a209
Ansh Shukla <anshukla@stanford.edu>
2013-07-10 11:06:07 -0700

    map.js: implemented mapping w/ underscore's interface

--- js/underscore/map.js
+++ js/underscore/map.js
@@ -3,6 +3,25 @@ var _ = {}
 var ArrayProto = Array.prototype;
 
 var nativeMap = ArrayProto.map;
+
 _.map = function (obj, iterator, context) {
-  
+  result = [];
+  if (nativeMap && obj.map === nativeMap) {
+    result = obj.map(iterator, context);
+  } else if (obj.length === +obj.length) {
+    for (var i = 0, l = obj.length; i < l; i++) {
+      result.push(iterator.call(context, obj[i], i, obj));
+    }
+  } else {
+    for (var key in obj) {
+      result.push(iterator.call(context, obj[key], key, obj));
+    }
+  }
+  return result;
 };
+
+console.log(_.map([1, 2, 3], function(num){ return num * 3; }));
+console.log(_.map({one : 1, two : 2, three : 3}, 
+  function(num, key){ 
+    return num * 3; 
+  }));
