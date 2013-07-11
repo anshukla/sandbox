588c4cca6a51b0f315338cdcda6ba1967b7c55ff
Ansh Shukla <anshukla@stanford.edu>
2013-07-11 11:44:46 -0700

    underscore: added reduce with minimal test cases

--- js/underscore/tests.js
+++ js/underscore/tests.js
@@ -1,3 +1,4 @@
+// load personal underscore library
 _ = require('./underscore_ansh.js')
 util = require('util')
 
@@ -11,3 +12,16 @@ console.log(_.map({one : 1, two : 2, three : 3},
     return num * 3; 
   }));
 
+// test reduce
+console.log(_.reduce([1, 2, 3], function(memo, num)
+                     { return memo + num; }, 0));
+
+console.log(_.reduce([0, 1, 2, 3, 4], function(previousValue, currentValue, 
+                                               index, array) {
+  return previousValue + currentValue;
+}));
+
+console.log(_.reduce([0, 1, 2, 3, 4], function(previousValue, currentValue, 
+                                               index, array) {
+  return previousValue + currentValue;
+}, 10));
--- js/underscore/underscore_ansh.js
+++ js/underscore/underscore_ansh.js
@@ -8,8 +8,10 @@ module.exports = _;
 
 var ArrayProto = Array.prototype;
 
+// clever approach stolen from underscore to save references to native functions
 var nativeForEach         = ArrayProto.forEach;
 var nativeMap             = ArrayProto.map;
+var nativeReduce          = ArrayProto.reduce;
 
 _.each = function(obj, iterator, context) {
   if (obj == null) return;
@@ -39,4 +41,54 @@ _.map = function (obj, iterator, context) {
     });
   }
   return result;
+};
+
+/*
+ * reduce(list, function, [memo, context])
+ * =======================================
+ * Reduce combines a list of values into a single value. "Memo" (also known as
+ * base) is the initial value and each successive step adds the value returned
+ * by the iterator to the memo. By default, the memo is set to 0. The iterator
+ * is called with the context and is expected to have the following signature:
+ *
+ * iterator(memo, value, index (key), list);
+ *
+ * The first time the iterator is called, memo can have one of two values: if
+ * memo was defined, then that value will be passed and value will be the first
+ * value in the array. Otherwise, memo will have the value of the first element
+ * in the array and value will be the second array element.
+ *
+ * If the list is null, reduce will throw a TypeError, consistent with the
+ * behavior of native reduce on null object.
+ *
+ * If an element of the list is undefined, it will be skipped.
+*/
+
+_.reduce = function (list, iterator, memo, context) {
+  // 1. figure out the right way to iterate through everything
+  //    a. Delegate that to the each method
+  //    b. If the native reduce function exists, call that
+  // 2. add memo to everything
+
+  // checks are optional – would be detected automatically at some point
+  // anyways. Taken from scrict mode of Firefox ECMA5 implementation.
+  if (list === null || typeof list === 'undefined')
+    throw new TypeError('_.reduce called on null or undefined');
+
+  if (typeof iterator !== 'function')
+    throw new TypeError(iterator + ' is not a function');
+
+  var initial = arguments.length > 2;
+
+  _.each(list, function(value, key, obj) {
+    if (!initial) {
+      memo = value;
+      initial = true;
+    }
+    else {
+      memo = iterator.call(context, memo, value, key, obj);
+    }
+  });
+  
+  return memo;
 };
